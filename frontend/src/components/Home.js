cb6766ce071ace330d2ffa1664b0da0be66e3162
boney <boneycanute@gmail.com>
2024-03-18 18:56:23 +0530

    v2

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useWeb3 } from './ConnectWallet'; // Import the useWeb3 hook to access web3 instance and other context variables
 import NFTMintDN404 from '../contracts/NFTMintDN404.json'
-import { Container, CardMedia, Grid, Card, CardContent, Link, Typography, Box} from '@mui/material';
+import { Container, CardMedia, Grid, Card, CardContent, Link, Typography, Box } from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
 
-
-
 function Home() {
-    const { web3js, nftFactoryContract, connected } = useWeb3(); // Destructure variables from the Web3 context
+    const { web3js, nftFactoryContract, connected, tokenContract } = useWeb3(); // Destructure variables from the Web3 context
     const [nftListings, setNftListings] = useState([]);
     const [loading, setLoading] = useState(false);
 
@@ -21,10 +19,10 @@ function Home() {
         try {
             setLoading(true);
             const allListings = await nftFactoryContract.methods.getAllContractAddresses().call();
-            
+
             // Flatten the 2D array to get a single array of contract addresses
             const flattenedAddresses = allListings.flat();
-    
+
             const listingsWithURI = await Promise.all(
                 flattenedAddresses.map(async (address) => {
                     const URI = await getURI(address);
@@ -32,7 +30,7 @@ function Home() {
                     return { nftAddress: address, URI, name };
                 })
             );
-    
+
             setNftListings(listingsWithURI);
             setLoading(false);
         } catch (error) {
@@ -60,40 +58,40 @@ function Home() {
 
     return (
         <Container maxWidth="md">
-        <Typography variant="overline" fontSize={40}>All NFTs minted</Typography>
-        {connected ? (
-            loading ? (
-                <Box>
-                <CircularProgress />
-                </Box>
-            ) : nftListings.length > 0 ? (
-                <Grid container spacing={2}>
-                    {nftListings.map((listing, index) => (
-                        <Grid item xs={12} sm={6} md={4} key={index}>
-                            <Link href={`https://nftmarketplace-a.vercel.app/${listing.nftAddress}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                                <Card sx={{ '&:hover': { boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.3)', cursor: 'pointer' } }}>
-                                    <CardMedia
-                                        component="img"
-                                        height="200"
-                                        image={listing.URI}
-                                        alt={`NFT ${index}`}
-                                    />
-                                    <CardContent sx={{ alignItems: "center", justifyContent: "center", textAlign: "center" }}>
-                                        <Typography variant="h5">{listing.name}</Typography>
-                                    </CardContent>
-                                </Card>
-                            </Link>
-                        </Grid>
-                    ))}
-                </Grid>
+            <Typography variant="overline" fontSize={40}>All NFTs minted</Typography>
+            {connected ? (
+                loading ? (
+                    <Box>
+                        <CircularProgress />
+                    </Box>
+                ) : nftListings.length > 0 ? (
+                    <Grid container spacing={2}>
+                        {nftListings.map((listing, index) => (
+                            <Grid item xs={12} sm={6} md={4} key={index}>
+                                <Link href={`https://nftmarketplace-a.vercel.app/${listing.nftAddress}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                    <Card sx={{ '&:hover': { boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.3)', cursor: 'pointer' } }}>
+                                        <CardMedia
+                                            component="img"
+                                            height="200"
+                                            image={listing.URI}
+                                            alt={`NFT ${index}`}
+                                        />
+                                        <CardContent sx={{ alignItems: "center", justifyContent: "center", textAlign: "center" }}>
+                                            <Typography variant="h5">{listing.name}</Typography>
+                                        </CardContent>
+                                    </Card>
+                                </Link>
+                            </Grid>
+                        ))}
+                    </Grid>
+                ) : (
+                    <p>No NFT minted</p>
+                )
             ) : (
-                <p>No NFT minted</p>
-            )
-        ) : (
-            <p>Connect your wallet to continue</p>
-        )}
-    </Container>
-);
+                <p>Connect your wallet to continue</p>
+            )}
+        </Container>
+    );
 }
 
 export default Home;
